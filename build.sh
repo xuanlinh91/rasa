94dc19037c3fab661508b999c7c9de6fa3f0ccca
Nguyen Xuan Linh <xuanlinh91@gmail.com>
2019-02-11 21:40:13 +0900

    fix error of training data folder

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 case "$1" in
 "train_nlu")
-  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_nlu:/app/models rasa/rasa_nlu:latest-tensorflow run python -m rasa_nlu.train -c config.yml -d project/data/ -o models --fixed_model_name nlu --project current
+  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_nlu:/app/models rasa/rasa_nlu:latest-tensorflow run python -m rasa_nlu.train -c config.yml -d project/data/nlu/ -o models --fixed_model_name nlu --project current
   ;;
 "train_core")
   docker rm -v $(docker ps -a -q -f status=exited)
-  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_core:/app/models -v $(pwd)/policy.yml:/app/policy.yml rasa/rasa_core:latest train --domain project/domain.yml --stories project/data/stories.md  -c policy.yml  --out models
+  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_core:/app/models -v $(pwd)/policy.yml:/app/policy.yml rasa/rasa_core:latest train --domain project/domain.yml --stories project/data/core/stories.md  -c policy.yml  --out models
   ;;
 "run")
   docker-compose up -d
