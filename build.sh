f89de25b979ecce9d0d51ab136768840d32b0560
Nguyen Xuan Linh <xuanlinh91@gmail.com>
2019-02-11 21:35:59 +0900

    fix models name

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 case "$1" in
 "train_nlu")
-  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_nlu:/app/models rasa/rasa_nlu:latest-tensorflow run python -m rasa_nlu.train -c config.yml -d project/data/nlu.md -o models --project current
-    ;;
+  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_nlu:/app/models rasa/rasa_nlu:latest-tensorflow run python -m rasa_nlu.train -c config.yml -d project/data/ -o models --fixed_model_name nlu --project current
+  ;;
 "train_core")
   docker rm -v $(docker ps -a -q -f status=exited)
-  docker run   -v $(pwd):/app/project   -v $(pwd)/models/rasa_core:/app/models -v $(pwd)/policy.yml:/app/policy.yml   rasa/rasa_core:latest   train     --domain project/domain.yml     --stories project/data/stories.md  -c policy.yml    --out models
-    ;;
+  docker run -v $(pwd):/app/project -v $(pwd)/models/rasa_core:/app/models -v $(pwd)/policy.yml:/app/policy.yml rasa/rasa_core:latest train --domain project/domain.yml --stories project/data/stories.md  -c policy.yml  --out models
+  ;;
 "run")
   docker-compose up -d
-    ;;
+  ;;
 "clean")
  docker rm -v $(docker ps -a -q -f status=exited)
-   ;;
+ ;;
 "stop")
  docker-compose down
-   ;;
+ ;;
 esac
